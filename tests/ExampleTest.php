4d3692e69bc5564dcb4aa5d3309b8821b6e33843
BorelEnzo <borelenzo@gmail.com>
2017-10-27 18:30:48 +0200

    Create basic test: file size

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,11 +1,10 @@
 <?php
 
 namespace Tests;
-
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
-{
+class ExampleTest extends TestCase{
+    
     /**
      * A basic test example.
      *
@@ -16,3 +15,4 @@ class ExampleTest extends TestCase
         $this->assertTrue(true);
     }
 }
+?>
