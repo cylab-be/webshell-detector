c3a0299e4fbeb603785490df09027486b75d42a2
Enzo <enzo@localhost.localdomain>
2018-01-27 11:59:09 +0100

    27.01.2018 Improve Exe analyzer - Anonymous functions

--- src/ExeAnalyzer.php
+++ src/ExeAnalyzer.php
@@ -36,40 +36,146 @@ class ExeAnalyzer implements Analyzer
      */
     public function analyze($string)
     {
-        $ret = $this->_searchExecCmdFunctions($string);
-        return $ret ? $ret : $this->_searchExecCmdFunctions(preg_replace('/<\?\n/', '<?php'.PHP_EOL, $string));
+        $string = preg_replace('/<\?\n/', '<?php'.PHP_EOL, $string);
+        $tokens = token_get_all($string);
+        if (count($tokens) === 0)
+            return 0;
+        $nbFunc = 0;
+        foreach ($tokens as $token) {
+            if (is_array($token) && $token[0] === T_STRING)
+                $nbFunc++;
+        }
+        //Util::printTokens($tokens);
+        $retExec = $this->_searchExecCmdFunctions($tokens);
+        $retAnonymous = $this->_searchAnonymousFunctions($tokens);
+        $retVarFunc = $this->_searchVariableFunctions($tokens);
+        echo PHP_EOL."Exec: $retExec \nAnonymous: $retAnonymous \nVariable Func: $retVarFunc";
+        return (($retExec * 2 + $retAnonymous + $retVarFunc) / 4) / $nbFunc;
     }
 
     /**
      * Basic. Searches dangerous function names allowing to execute commands
      * 
-     * @param string $string The string to analyze
+     * @param array $tokens Tokens of the code to analyze
      *
      * @return int. Number of dangerous functions
      */
-    private function _searchExecCmdFunctions($string)
+    private function _searchExecCmdFunctions($tokens)
     {
         $count = 0;
-        $nbroutines = 0;
-        $tokens = token_get_all($string);
-        if (count($tokens) === 0) {
-            return 0;
-        }
-        $funcs = array("exec", "passthru", "popen", "proc_open", "pcntl_exec", "shell_exec", "system");
+        $funcs = array("exec", "assert", "passthru", "popen", "proc_open", "pcntl_exec", "shell_exec", "system");
         foreach ($tokens as $token) {
             if (is_array($token) && $token[0] === T_STRING) {
-                $nbroutines++;
-                foreach ($funcs as $func) {
-                    if ($token[1] === $func) {
-                        $count++;
-                    }
-                }
+                if (in_array($token[1], $funcs))
+                    $count++;
             }
             elseif ($token[0] === T_EVAL)
                 $count++;
             elseif ($token === "`")
                 $count+=0.5;
         }
-        return $nbroutines ? $count/$nbroutines : 0.0;
+        return $count;
+    }
+    
+    /**
+     * Searches anonymous, user-defined and callback functions
+     * 
+     * @param array $tokens Tokens of the code
+     * 
+     * @return number Number and callback, user-defined or anonymous functions
+     */
+    private function _searchAnonymousFunctions($tokens)
+    {
+        $count = 0;
+        $funcs = array(
+            "array_diff_uassoc",
+            "array_diff_ukey",
+            "array_filter",
+            "array_intersect_uassoc",
+            "array_intersect_ukey",
+            "array_map",
+            "array_reduce",
+            "array_udiff",
+            "array_udiff_assoc",
+            "array_uintersect",
+            "array_uintersect_assoc",
+            "array_uintersect_uassoc",
+            "call",
+            "call_user_func",
+            "call_user_func_array",
+            "create_function",
+            "forward_static_call",
+            "forward_static_call_array", 
+            "preg_replace_callback",
+            "preg_replace_callback_array",
+            "register_shutdown_function",
+            "register_tick_function",
+            "set_exception_handler", 
+            "usort");
+        foreach ($tokens as $token) {
+            //echo (is_array($token) ? $token[1] : $token).PHP_EOL;
+            if (is_array($token) && $token[0] === T_STRING) {
+                if (in_array($token[1], $funcs))
+                    $count++;
+            }
+        }
+        return $count;
+    }
+    
+    /**
+     * Looks for variable functions, such as:
+     * $foo = function() {...}
+     * $foo()
+     * OR
+     * $foo = 'some_function_name';
+     * $foo()
+     * 
+     * @param array $tokens Tokens of the code
+     * 
+     * @return number Number of variable functions
+     */
+    private function _searchVariableFunctions($tokens)
+    {
+        $state = 0;
+        $matches = 0;
+        $nestedParenthesis = 0;
+        //$stateStr = '';
+        for ($i = 0; $i < count($tokens); $i++) {
+            $token = $tokens[$i];
+            if ($state == 0 && is_array($token) && $token[0] === T_VARIABLE) {
+                //look for variable
+                $state = 1;
+                //$stateStr.= $token[1];
+            } elseif ($state == 1) {
+                //look for left parenthesis
+                if (is_array($token) && $token[0] === T_WHITESPACE)
+                    continue;
+                elseif ($token === "(") {
+                    $state = 2;
+                    //$stateStr.='(';
+                } else {
+                    $state = 0;
+                    //$stateStr = '';
+                }
+            } elseif ($state == 2) {
+                //look for param
+                if (is_array($token) && $token[0] === T_WHITESPACE)
+                    continue;
+                elseif ($token === "(")
+                    $nestedParenthesis++;
+                elseif ($token === ")") {
+                    if ($nestedParenthesis != 0)
+                        $nestedParenthesis--;
+                    else {
+                        $state = 0;
+                        //echo PHP_EOL.$stateStr.")";
+                        //$stateStr = '';
+                        $matches++;
+                    }
+                }
+            }
+        }
+        //TODO can't handle nested func
+        return $matches;
     }
 }
--- tests/res/test.php
+++ tests/res/test.php
@@ -7,3 +7,13 @@ $Æ = "test";
 echo $Æ;
 $exec= `ls -al`;
 exec("date");
+assert("phpinfo()");
+$a = function($param){print($param);};
+$$Æ( 
+    $param(), $param1, "
+)");
+$func_name = "phpinfo";
+$func_name
+
+(
+    );
