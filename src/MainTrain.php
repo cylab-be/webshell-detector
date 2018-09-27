59ea6dfa2069f0d3ffbdb84697d40780d4abbac3
a.croix <croix.alexandre@gmail.com>
2018-09-27 14:53:13 +0200

    Adaptation of webshell to use wowa-training

---
+++ src/MainTrain.php
@@ -0,0 +1,50 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require_once "vendor/autoload.php";
+
+use RUCD\WebshellDetector\TrainDetector;
+use RUCD\Training\Trainer;
+use RUCD\Training\TrainerParameters;
+use Aggregation\WOWA;
+
+
+error_reporting(E_ERROR);
+
+$dataFile = tempnam(__DIR__ . "/../trainer_file", 'data_file_');
+$expectedFile = tempnam(__DIR__ . "/../trainer_file", 'expected_file');
+$weights = tempnam(__DIR__ . "/../trainer_file", 'weights_');
+
+$detector = new TrainDetector();
+echo __DIR__;
+echo "\n";
+$data = iterator_to_array($detector->analyzeDirectory(__DIR__ . "/../tests", $dataFile, $expectedFile ));
+//$data = $detector->analyzeDirectory("/home/alex/Projects/webshell-detector/tests/res/webshells_modified");
+foreach ($data as $key => $score) {
+    echo "$score : $key \n";
+    //var_dump($score);
+}
+
+
+var_dump(unserialize(file_get_contents($dataFile)));
+var_dump(unserialize(file_get_contents($expectedFile)));
+
+$populationSize = 120;
+$crossoverRate = 50;
+$mutationRate = 19;
+$selectionMethod = TrainerParameters::SELECTION_METHOD_RWS;
+$maxGenerationNumber = 120;
+$parameters = new TrainerParameters(null, $populationSize, $crossoverRate, $mutationRate, $selectionMethod, $maxGenerationNumber);
+$trainer = new Trainer($parameters);
+$result = $trainer->run(unserialize(file_get_contents($dataFile)), unserialize(file_get_contents($expectedFile)));
+file_put_contents($weights, serialize($result));
+var_dump($result);
+
+foreach(unserialize(file_get_contents($dataFile)) as $line) {
+    echo WOWA::wowa($result->weights_w, $result->weights_p, $line);
+    echo "\n";
+}
---
+++ src/TrainDetector.php
@@ -0,0 +1,124 @@
+<?php
+namespace RUCD\WebshellDetector;
+
+
+use Aggregation\WOWA;
+/**
+ * Class Detector. Entry point containing all analyzers
+ *
+ * @file     Dectector
+ * @category None
+ * @package  Source
+ * @author   Thibault Debatty <thibault.debatty@gmail.com>
+ * @license  https://github.com/RUCD/webshell-detector/blob/master/LICENSE MIT
+ * @link     https://github.com/RUCD/webshell-detector
+ */
+class TrainDetector
+{
+
+    private $_analyzers = [];
+
+    /**
+     * Contructor of Detector. Initializes analyzers
+     */
+    public function __construct()
+    {
+        $this->_analyzers[] = new ExeAnalyzer();
+        $this->_analyzers[] = new SignaturesAnalyzer();
+        $this->_analyzers[] = new EntropyAnalyzer();
+        $this->_analyzers[] = new FuzzyHashingAnalyzer();
+        $this->_analyzers[] = new ObfuscationAnalyzer();
+    }
+    
+
+    /**
+     * Recursively scan a directory, and scan all files.
+     *
+     * @param string $directory Name of the directory to scan
+     *
+     * @return array a generator of entries $file => $score
+     */
+    public function analyzeDirectory($directory, $dataFile, $expectedFile)
+    {
+
+        if (!is_dir($directory)) {
+            throw new Exception("$directory is not a directory!");
+        }
+        
+        $files = scandir($directory);
+        foreach ($files as $file) {
+            if ($file === ".." || $file === ".") {
+                continue;
+            }
+
+            $file = $directory . DIRECTORY_SEPARATOR . $file;
+            if (is_dir($file)) {
+                yield from $this->analyzeDirectory($file, $dataFile, $expectedFile);
+                continue;
+
+            }
+
+            if (preg_match('/\.php$/', $file)) {
+                if(preg_match('~/res/~', $file)) {
+                    $this->_addDataToFile($expectedFile, 1);
+                }
+                else {
+                    $this->_addDataToFile($expectedFile, 0);
+                }
+                $score = $this->analyzeString(
+                    file_get_contents($file), $dataFile
+                );
+                yield $file => $score;
+            }
+        }
+    }
+
+    /**
+     * Analyze a string and return a score between 0 (harmless) and 1 (highly
+     * suspicious).
+     *
+     * @param string $string The string to analyzer
+     *
+     * @return float The score
+     */
+
+    
+    public function analyzeString($string, $fileToStore)
+    {
+        $scores = [];
+        foreach ($this->_analyzers as $analyzer) {
+            $res = $analyzer->analyze($string);
+            $scores[] = $res;
+        }
+        $this->_addDataToFile($fileToStore, $scores);
+        return $this->_aggregate($scores);
+    }
+
+    /**
+     * Returns a mark regarding the harmfulness of submitted scores
+     *
+     * @param array $scores The computed scores
+     *
+     * @return float The final score
+     */
+    private function _aggregate($scores)
+    {
+        // Wowa aggregation
+        return array_sum($scores) / count($scores);
+        //return WOWA::wowa($this->w, $this->p, $scores);
+    }
+    
+    private function _addDataToFile($fileName, $data) {
+        if (!file_exists($fileName)) {
+            $dataArray = [];
+            $dataArray[] = $data;
+            file_put_contents($fileName, serialize($dataArray));
+        }
+        else {
+            $oldData = unserialize(file_get_contents($fileName));
+            $oldData[] = $data;
+            file_put_contents($fileName, serialize($oldData));
+            
+        }
+    }
+}
