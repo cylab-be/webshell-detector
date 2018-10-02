5d2ba2e11c6eeb0db44c4717967e3882575be2f5
a.croix <croix.alexandre@gmail.com>
2018-10-02 15:28:23 +0200

    Correction for store results in file

--- src/MainTrain.php
+++ src/MainTrain.php
@@ -69,10 +69,11 @@ var_dump($result);
 file_put_contents('Comparison.txt', "Expected       WOWA        Average\n");
 for ($i = 0; $i < count($data); $i++) {
     $wowa = WOWA::wowa($result->weights_w, $result->weights_p, unserialize(file_get_contents($dataFile))[$i]);
-    $average = array_sum($data[$i]) / count($data[$i]);
+    $average = array_sum(unserialize(file_get_contents($dataFile))[$i]) / count(unserialize(file_get_contents($dataFile))[$i]);
+    $expected = unserialize(file_get_contents($expectedFile))[$i];
     file_put_contents(
         'Comparison.txt',
-        "$expected[$i]      $wowa   $average\n",
+              "$expected    $wowa   $average\n",
         FILE_APPEND
     );
     if ($expected[$i] === 1) {
