9075b88135afbd88ceb23c12ce22b1d0b7b262c5
a.croix <croix.alexandre@gmail.com>
2018-10-02 14:30:21 +0200

    Webshell + trainer for test on runner with package from packagist

--- src/MainTrain.php
+++ src/MainTrain.php
@@ -41,41 +41,41 @@ echo "PHP files analyzed !\n";
 $fileNumber = count(unserialize(file_get_contents($dataFile)));
 echo "There are : $fileNumber analyzed!\n";
 
-//$logger = new Logger('wowa-training-test');
-//$logger->pushHandler(new StreamHandler('php://stdout', Logger::DEBUG));
-//
-//
-//$populationSize = 120;
-//$crossoverRate = 50;
-//$mutationRate = 19;
-//$selectionMethod = TrainerParameters::SELECTION_METHOD_RWS;
-//$maxGenerationNumber = 120;
-//$parameters = new TrainerParameters(
-//    $logger, 
-//    $populationSize, 
-//    $crossoverRate, 
-//    $mutationRate, 
-//    $selectionMethod, 
-//    $maxGenerationNumber
-//);
-//$trainer = new Trainer($parameters);
-//$result = $trainer->run(
-//    unserialize(file_get_contents($dataFile)), 
-//    unserialize(file_get_contents($expectedFile))
-//);
-//file_put_contents($weights, serialize($result));
-//var_dump($result);
-//
-//file_put_contents('Comparison.txt', "Expected       WOWA        Average\n");
-//for ($i = 0; $i < count($data); $i++) {
-//    $wowa = WOWA::wowa($result->weights_w, $result->weights_p, $data[$i]);
-//    $average = array_sum($data[$i]) / count($data[$i]);
-//    file_put_contents(
-//        'Comparison.txt',
-//        "$expected[$i]      $wowa   $average\n",
-//        FILE_APPEND
-//    );
-//    if ($expected[$i] === 1) {
-//        echo "$expected[$i] : $wowa : $average\n";
-//    }  
-//}
+$logger = new Logger('wowa-training-test');
+$logger->pushHandler(new StreamHandler('php://stdout', Logger::DEBUG));
+
+
+$populationSize = 120;
+$crossoverRate = 50;
+$mutationRate = 19;
+$selectionMethod = TrainerParameters::SELECTION_METHOD_RWS;
+$maxGenerationNumber = 120;
+$parameters = new TrainerParameters(
+    $logger, 
+    $populationSize, 
+    $crossoverRate, 
+    $mutationRate, 
+    $selectionMethod, 
+    $maxGenerationNumber
+);
+$trainer = new Trainer($parameters);
+$result = $trainer->run(
+    unserialize(file_get_contents($dataFile)), 
+    unserialize(file_get_contents($expectedFile))
+);
+file_put_contents($weights, serialize($result));
+var_dump($result);
+
+file_put_contents('Comparison.txt', "Expected       WOWA        Average\n");
+for ($i = 0; $i < count($data); $i++) {
+    $wowa = WOWA::wowa($result->weights_w, $result->weights_p, $data[$i]);
+    $average = array_sum($data[$i]) / count($data[$i]);
+    file_put_contents(
+        'Comparison.txt',
+        "$expected[$i]      $wowa   $average\n",
+        FILE_APPEND
+    );
+    if ($expected[$i] === 1) {
+        echo "$expected[$i] : $wowa : $average\n";
+    }  
+}
